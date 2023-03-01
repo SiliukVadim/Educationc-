30296457eeec3403b6e15cee4fb67000e2e01213
Siliuk Vadim <skrug07@mail.ru>
2023-03-01 17:10:38 +0300

    Homeworks6

---
+++ Homeworks/Homeworks6/Program.cs
@@ -0,0 +1,160 @@
+﻿// // Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// // [345, 897, 568, 234] -> 2
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// void Chet(int[] array)
+// {
+//     int colchet = 0;
+//     for(int i = 0;i < array.Length;i++)
+//     {
+//         if(array[i] % 2 == 0) colchet += 1;
+//     }
+//     Console.WriteLine($"Количество четных чисел -> {colchet}");
+// } 
+// Console.WriteLine("Ввести размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Минимальное значение");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Максимальное значение");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// Chet(myArray);
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// void SumChet(int[] array)
+// {
+//     int sum = 0;
+//     for(int i = 0;i < array.Length;i++)
+//     {
+//         if(i % 2 == 0) sum = sum;
+//         else sum = array[i] + sum;
+//     }
+//     Console.WriteLine($"Сумма чисел на не чётных позициях -> {sum}");
+// } 
+// Console.WriteLine("Ввести размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Минимальное значение");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Максимальное значение");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// SumChet(myArray);
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+//////Целые числа
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// void Raz(int[] array)
+// {
+//     int min = array[0];
+//     int max = 0;
+//     for(int i = 0;i < array.Length;i++)
+//     {
+//         if(array[i] > max) max = array[i];
+//         if(array[i] < min) min = array[i];
+//     }
+//     Console.WriteLine($"Разница между {min} и {max} {max - min}");
+// } 
+// Console.WriteLine("Ввести размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Минимальное значение");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Максимальное значение");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// Raz(myArray);
+
+////// ВЕЩЕСТВЕННЫЕ ЧИСЛА
+
+double[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    double[] array = new double[size];
+    for(int i = 0;i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1) + Math.Round(new Random().NextDouble());
+    }
+    return array;
+}
+void ShowArray (double[] array)
+{
+    for(int i = 0;i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+void Raz(double[] array)
+{
+    double min = array[0];
+    double max = 0;
+    for(int i = 0;i < array.Length;i++)
+    {
+        if(array[i] > max) max = array[i];
+        if(array[i] < min) min = array[i];
+    }
+    Console.WriteLine($"Разница между {min} и {max} {max - min}");
+} 
+Console.WriteLine("Ввести размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Минимальное значение");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное значение");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+double[] myArray = CreateRandomArray(size, minValue, maxValue);
+ShowArray(myArray);
+Raz(myArray);
---
+++ seminar/seminar6/Program.cs
@@ -0,0 +1,164 @@
+﻿// //Задача 1. Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// //а первый - на последнем и т.д.)
+// //[1 2 3 4 5] -> [5 4 3 2 1]
+// //[6 7 3 6] -> [6 3 7 6]
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0;i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// int[] ReverseArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+//         int temp = array[i];
+//         array[i] = array[array.Length - i - 1];
+//         array[array.Length - i - 1] = temp;
+//     }
+
+//     return array;
+
+// }
+
+
+// Console.WriteLine("Input array size");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, minValue, maxValue);
+// ShowArray(myArray);
+// int[] reversedArray = ReverseArray(myArray);
+// ShowArray(reversedArray);
+
+//Задача 2.Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+//самое важное знать правило, какждая сторона должна быть меньше(строго) суммы двух других сторон
+
+// bool CheckTriangle(int x, int y, int z)
+// {
+//     if (x<y+z && y<z+x && z<x+y) 
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+// Console.WriteLine("Длина а");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Длина b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Длина c");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($" {a}  {b}  {c} ? {CheckTriangle(a, b, c)}");
+
+//Задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: a и b.
+//числа фибоначчи последовательность чисел в которой первые 2 числа это 0 и 1, а последубщие это сумма двух передыдущих
+//[0, 1,2, 3, 5, 8, 13, ... ]
+
+// int[] Fibonachi(int size, int a, int b)
+// {
+//     int[] array = new int[size];
+//     array[0] = a;
+//     array[1] = b;
+//     for(int i = 2;i < size; i++)
+//     {
+//         array[i] = array[i -2] + array[i - 1];
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Длина ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// ShowArray(Fibonachi(a, b, size));
+///////
+// int[] Fibonachi (int a, int b, int lenght)
+// {
+//     int[] array = new int[lenght];
+//     array[0] = a;
+//     array[1] = b;
+//     for (int i = 2; i < lenght; i++)
+//     {
+//         array[i]= array[i - 2] + array[i - 1];
+//     }
+//     return array;
+// }
+// void ShowArray (int[] array)
+// {
+//     for(int i = 0;i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Input number of a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input lenght");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// ShowArray(Fibonachi(a, b, lenght));
+
+
+//////
+//Задача 4. ****Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+
+void Two (int number)
+{
+    string current = String.Empty;
+    while (number > 0)
+    {
+        current = number % 2 + current;
+        number = number / 2;
+    }
+    Console.WriteLine(current);
+}
+Console.WriteLine("number");
+int number = Convert.ToInt32(Console.ReadLine());
+Two(number);
+
+// void Two (int number)
+// {
+//     string current = String.Empty;
+//     int currentnumber = 0;
+//     while (number > 0)
+//     {
+//         current = current + number % 2;
+//         number = number / 2;
+//     }
+//     Console.WriteLine(current);
+// }
+// Console.WriteLine("Input number");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Two(number);
