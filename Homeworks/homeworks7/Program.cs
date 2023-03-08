b2bae906cc19e2c916d48405b5b2504232bd1934
Siliuk Vadim <skrug07@mail.ru>
2023-03-08 20:20:20 +0300

    homeworks7

---
+++ Homeworks/homeworks7/Program.cs
@@ -0,0 +1,105 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// int Prompt (string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+// int[] InputArray(int length)
+// {
+//     int[] array = new int[length];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = Prompt($"ВВедите {i+1}-ый элемент");
+//     }
+//     return array;
+// }
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine($"a[{i}] = {array[i]}");
+//     }
+// }
+// int CountPositiveNumbers (int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// int lenght = Prompt("Введите количеств элементов >");
+// int[] array;
+// array = InputArray(lenght);
+// PrintArray(array);
+// Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int koef = 0;
+const int constant = 1;
+const int x_cord = 0;
+const int y_cord = 1; 
+const int line1 = 1;
+const int line2 = 2;
+
+double[] lineData1 = InputLineData(line1);
+double[] lineData2 = InputLineData(line2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($" Точка пересечения уравнений y={lineData1[koef]}*x+{lineData1[constant]} и y={lineData2[koef]}*x+{lineData2[constant]}");
+    Console.WriteLine($" имеет кооринаты ({coord[x_cord]}, {coord[y_cord]})");
+}
+double Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+double[] InputLineData(int numberOfline)
+{
+    double[] lineData = new double[2];
+    lineData[koef] = Prompt($"Введите коэффициент для {numberOfline} прямой >");
+    lineData[constant] = Prompt($"Введите коэффициент для {numberOfline} прямой >");
+    return lineData;
+}
+double[] FindCoords (double[] lineData1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[x_cord] = (lineData1[constant] - lineData2[constant]) / (lineData1[koef] - lineData2[koef]);
+    coord[y_cord] = lineData1[constant] * coord[x_cord] + lineData1[constant];
+    return coord;
+}
+
+bool ValidateLines(double[] lineData1, double[] lineData2)
+{
+    if (lineData1[koef] == lineData2[koef])
+    {
+        if (lineData1[constant] == lineData2[constant])
+        {
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Прямые параллельны");
+        return false;
+    }
+    return true;
+}
---
+++ seminar/seminar7/Program.cs
@@ -0,0 +1,216 @@
+﻿// //Задача 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+// int[,] Create2DRandomArray(int colums, int rows, int minValue, int maxValue)
+// {
+//     int[,] newArray = new int[rows, colums];
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colums; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue+1);
+
+//         }
+//     }
+
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Input number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[,] array = Create2DRandomArray(columns, rows, minValue, maxValue);
+// Show2DArray(array);
+
+
+//Задача 2. ****Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n.
+// Выведите полученный массив на экран.
+
+// int[,] Create2DArray(int colums, int rows)
+// {
+//     int[,] newArray = new int[rows, colums];
+
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < colums; j++)
+//         {
+//             newArray[i, j] = i+j;
+//         }
+//     }
+
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Console.WriteLine("Input number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// Show2DArray(Create2DArray(columns, rows));
+
+
+
+
+//Задача 3.Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+//Например, изначально массив выглядел вот так:
+
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Новый массив будет выглядеть вот так:
+
+//1 4 7 2
+//5 **81  ** 2 **9**
+//8 4 2 4
+
+// int[,] Create2DRandomArray(int columns,
+//  int rows, int minValue, int maxValue)
+// {
+//     int [,] newArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue);
+//         }
+        
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int [,] DArray (int[,] array)
+// {
+//     for (int i = 0; i<array.GetLength(0); i+=2)
+//     {
+//         for (int j = 0; j<array.GetLength(1); j+=2)
+//         {
+//             array[i, j] = array[i,j]* array[i,j];
+//         }
+//     }
+// return array;
+// }
+
+// Console.WriteLine("Input number of rows");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of columns");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[,]array = Create2DRandomArray(columns,rows,minValue,maxValue);
+// Show2DArray(array);
+// Show2DArray(DArray(array));
+
+// //Задача 4. адайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+// //Например, задан массив:
+
+// //1 4 7 2                      Главная диагональ это когда i и j совпадают
+// //5 9 2 3
+// //8 4 2 4
+
+// //Сумма элементов главной диагонали: 1+9+2 = 12
+
+// int[,] Create2DRandomArray(int columns,
+//  int rows, int minValue, int maxValue)
+// {
+//     int [,] newArray = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             newArray[i, j] = new Random().Next(minValue, maxValue);
+//         }
+        
+//     }
+//     return newArray;
+// }
+
+// void Show2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// int Diagonal (int[,] array)
+// {
+//     int sum = 0;
+//     for (int i = 0; i<array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j<array.GetLength(1); j++)
+//         {
+//             if (i==j)
+//             {
+//                 sum = array[i, j] + sum;
+//             }
+//         }
+//     }
+// return sum;
+// }
+
+// Console.WriteLine("Input number of rows");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of columns");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input minimal value of array element");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value of array element");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[,]array = Create2DRandomArray(columns,rows,minValue,maxValue);
+// Show2DArray(array);
+// Console.WriteLine(Diagonal(array));
